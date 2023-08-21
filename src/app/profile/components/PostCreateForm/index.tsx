57e8a1e34b98119387de63b11c6e2c544a905c23
Max <makasyslar@gmail.com>
2023-08-21 16:26:16 +0300

    Local changes

--- src/app/profile/components/PostCreateForm/index.tsx
+++ src/app/profile/components/PostCreateForm/index.tsx
@@ -29,6 +29,7 @@ const PostCreateForm: FC<PostCreateFormProps> = (props) => {
 				</p>
 				<label className="w-full" htmlFor="postTitle">
 					<Input
+						className="peer"
 						id="postTitle"
 						name="postTitle"
 						placeholder="Enter post title"
@@ -48,8 +49,9 @@ const PostCreateForm: FC<PostCreateFormProps> = (props) => {
 					Photo<span>*</span>
 				</p>
 				<div className="flex gap-3">
-					<label htmlFor="postImg" className="w-full">
+					<label htmlFor="postImg" className="peer w-full">
 						<Input
+							className="peer"
 							id="postImg"
 							name="postImg"
 							placeholder="Paste image url"
@@ -69,7 +71,7 @@ const PostCreateForm: FC<PostCreateFormProps> = (props) => {
 				</p>
 				<label className="w-full" htmlFor="postText">
 					<textarea
-						className="peer min-h-[12.5rem] w-full rounded-lg border px-4 py-2 text-base text-black outline-none outline-transparent valid:border-red-500 active:border-blue-800"
+						className="peer min-h-[12.5rem] w-full rounded-lg border border-primary bg-white px-4 py-2 text-base text-primary outline-none outline-transparent  duration-200 focus:bg-primary focus:text-white dark:border-white dark:bg-primary dark:text-white dark:focus:bg-white dark:focus:text-primary"
 						id="postText"
 						name="postText"
 						placeholder="Enter your text"
@@ -90,7 +92,7 @@ const PostCreateForm: FC<PostCreateFormProps> = (props) => {
 				<div className="">
 					<label className="w-full" htmlFor="postTag">
 						<Input
-							className="mb-4"
+							className="mb-4 peer"
 							id="postTag"
 							name="postTag"
 							placeholder="Enter your tags"
@@ -100,9 +102,9 @@ const PostCreateForm: FC<PostCreateFormProps> = (props) => {
 							onChange={props.handleTagsChange}
 							required
 						/>
-						<p className="mt-1 hidden text-sm font-medium text-pink-600 peer-invalid:block peer-focus:hidden">
+						{/* <p className="mt-1 hidden text-sm font-medium text-pink-600 peer-invalid:block peer-focus:hidden">
 							Provide at least 3 letters
-						</p>
+						</p> */}
 					</label>
 					<Button onClick={props.showPreviewTags}>Add tag</Button>
 				</div>
--- src/ui/Title/index.tsx
+++ src/ui/Title/index.tsx
@@ -6,7 +6,7 @@ type TitleProps = {
 
 const Title: React.FC<TitleProps> = ({ titleText }: TitleProps) => {
 	return (
-		<h1 className="xs:text-[2.5rem] xxs:text-[2rem] border-y-2 border-primary text-center text-[14rem] font-bold uppercase leading-tight dark:border-secondary sm:text-[4rem] md:text-[6rem] lg:text-[8rem] xl:text-[10rem]">
+		<h1 className="text-[2.5rem] xxs:text-[2rem] border-y-2 border-primary text-center 2xl:text-[14rem] font-bold uppercase leading-tight dark:border-secondary sm:text-[4rem] md:text-[6rem] lg:text-[8rem] xl:text-[10rem]">
 			{titleText}
 		</h1>
 	);
